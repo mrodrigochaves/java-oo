c5b432c2a4af4ee3692f64b43312cb753f8c233a
mrodrigochaves <dev.marciorodrigo@gmail.com>
2023-03-28 15:06:02 -0300

    first commit

---
+++ src/edu/mrodrigochaves/exercices/car/Car.java
@@ -0,0 +1,49 @@
+package edu.mrodrigochaves.exercices.car;
+
+public class Car {
+
+    String color;
+    String model;
+    int tankCapacity;
+
+    Car() {
+
+    }
+
+    Car(String color, String model, int tankCapacity) {
+        this.color = color;
+        this.model = model;
+        this.tankCapacity = tankCapacity;
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getTankCapacity() {
+        return tankCapacity;
+    }
+
+    public void setTankCapacity(int tankCapacity) {
+        this.tankCapacity = tankCapacity;
+    }
+
+    double totalTankValue(double fuelValue) {
+        return tankCapacity * fuelValue;
+
+    }
+
+}
